f1b5237f8444559d534bf20cc2f2da9c818c40e0
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2020-06-22 10:30:24 +0200

    Tokens for storing and easily handling tokens.

---
+++ HanabiSolver.Library.Tests/TokensTests.cs
@@ -0,0 +1,82 @@
+﻿using FluentAssertions;
+using HanabiSolver.Library.Game;
+using System;
+using Xunit;
+
+namespace HanabiSolver.Library.Tests
+{
+	public class TokensTests
+	{
+		[Fact]
+		public void ConstructorSetsMaxAmount()
+		{
+			var tokens = new Tokens(3);
+
+			tokens.MaxAmount.Should().Be(3);
+		}
+
+		[Fact]
+		public void ConstructorSetsAmount()
+		{
+			var tokens = new Tokens(3);
+
+			tokens.Amount.Should().Be(3);
+		}
+
+		[Theory]
+		[InlineData(0)]
+		[InlineData(-1)]
+		public void ConstructorThrowsForNonPositiveMaxAmount(int maxAmount)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => new Tokens(maxAmount));
+		}
+
+		[Theory]
+		[InlineData(-1)]
+		[InlineData(4)]
+		public void ConstructorThrowsForOutOfRangeAmount(int amount)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => new Tokens(3, amount));
+		}
+
+		[Fact]
+		public void RemoveDecreasesAmount()
+		{
+			var tokens = new Tokens(3);
+
+			tokens.Remove();
+
+			tokens.Amount.Should().Be(2);
+		}
+
+		[Fact]
+		public void RemoveFromEmptyThrowsException()
+		{
+			var tokens = new Tokens(3, 0);
+
+			tokens
+				.Invoking(tokens => tokens.Remove())
+				.Should().Throw<InvalidOperationException>();
+		}
+
+		[Fact]
+		public void AddIncreasesAmount()
+		{
+			var tokens = new Tokens(3, 0);
+
+			tokens.Add();
+
+			tokens.Amount.Should().Be(1);
+		}
+
+		[Fact]
+		public void AddToFullThrowsException()
+		{
+			var tokens = new Tokens(3);
+
+			tokens
+				.Invoking(tokens => tokens.Add())
+				.Should().Throw<InvalidOperationException>();
+		}
+	}
+}
---
+++ HanabiSolver.Library/Game/Tokens.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HanabiSolver.Library.Game
+{
+	public class Tokens
+	{
+		public int MaxAmount { get; }
+
+		public int Amount { get; private set; }
+
+		public Tokens(int maxAmount)
+			: this(maxAmount, maxAmount)
+		{
+			MaxAmount = maxAmount;
+			Amount = maxAmount;
+		}
+
+		public Tokens(int maxAmount, int amount)
+		{
+			MaxAmount = (0 < maxAmount)
+				? maxAmount
+				: throw new ArgumentOutOfRangeException(nameof(maxAmount));
+
+			Amount = ((0 <= amount) && (amount <= maxAmount))
+				? amount
+				: throw new ArgumentOutOfRangeException(nameof(amount));
+		}
+
+		public void Remove()
+		{
+			if (Amount <= 0)
+				throw new InvalidOperationException();
+
+			Amount -= 1;
+		}
+
+		public void Add()
+		{
+			if (Amount >= MaxAmount)
+				throw new InvalidOperationException();
+
+			Amount += 1;
+		}
+	}
+}
