54e683d734a7ebdca93d244e75df3fbe1e0b2ef4
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2020-09-20 11:11:23 +0200

    EnumerableExtensions.None(), and tests for EnumerableExtensions.

---
+++ HanabiSolver.Library.Tests/Extensions/EnumerableExtensionsTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using HanabiSolver.Library.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace HanabiSolver.Library.Tests.Extensions
+{
+	public class EnumerableExtensionsTests
+	{
+		[Fact]
+		public void AsEnumerableHasTheSameElements()
+		{
+			var character = 'c';
+
+			var enumerable = character.AsEnumerable();
+
+			var expectedValues = new List<char> { 'c' };
+			enumerable.Should().BeEquivalentTo(expectedValues);
+		}
+
+		[Fact]
+		public void ExceptAtSkipsTheElementAtIndexUsed()
+		{
+			var characters = new List<char> { 'a', 'b', 'c' };
+
+			var values = characters.ExceptAt(1);
+
+			var expectedValues = new List<char> { 'a', 'c' };
+			values.Should().BeEquivalentTo(expectedValues);
+		}
+
+		[Fact]
+		public void NoneIsTrueForEmpty()
+		{
+			var empty = Enumerable.Empty<char>();
+
+			var none = empty.None();
+
+			none.Should().BeTrue();
+		}
+
+		[Fact]
+		public void NoneIsFalseForNonEmpty()
+		{
+			var some = new List<char> { 'a' };
+
+			var none = some.None();
+
+			none.Should().BeFalse();
+		}
+
+		[Fact]
+		public void NoneWithPredicateIsTrueForEmpty()
+		{
+			var empty = Enumerable.Empty<char>();
+
+			var none = empty.None(c => c == 'x');
+
+			none.Should().BeTrue();
+		}
+
+		[Fact]
+		public void NoneWithPredicateIsTrueForListFulfillingThePredicate()
+		{
+			var some = new List<char> { 'a', 'b', 'c' };
+
+			var none = some.None(c => c == 'x');
+
+			none.Should().BeTrue();
+		}
+
+		[Fact]
+		public void NoneWithPredicateIsFalseForListFailingThePredicate()
+		{
+			var some = new List<char> { 'a', 'x', 'c' };
+
+			var none = some.None(c => c == 'x');
+
+			none.Should().BeFalse();
+		}
+	}
+}
--- HanabiSolver.Library/Extensions/EnumerableExtensions.cs
+++ HanabiSolver.Library/Extensions/EnumerableExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace HanabiSolver.Library.Extensions
@@ -13,6 +14,16 @@ namespace HanabiSolver.Library.Extensions
 		public static IEnumerable<TSource> ExceptAt<TSource>(this IEnumerable<TSource> source, int index)
 		{
 			return source.Where((element, i) => i != index);
+		}
+
+		public static bool None<TSource>(this IEnumerable<TSource> source)
+		{
+			return !source.Any();
+		}
+
+		public static bool None<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+		{
+			return !source.Any(predicate);
 		}
 	}
 }
