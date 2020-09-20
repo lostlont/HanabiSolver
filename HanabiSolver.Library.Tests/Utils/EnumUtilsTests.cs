d1ca94db2ca21af29a74de6547751d8a3249ed14
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2020-09-20 11:17:49 +0200

    EnumUtils are tested.

---
+++ HanabiSolver.Library.Tests/Utils/EnumUtilsTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using HanabiSolver.Library.Utils;
+using System.Collections.Generic;
+
+namespace HanabiSolver.Library.Tests.Utils
+{
+	public class EnumUtilsTests
+	{
+		private enum Test
+		{
+			A,
+			B,
+			C,
+		}
+
+		public void ValuesProvidesValuesList()
+		{
+			var values = EnumUtils.Values<Test>();
+
+			var expectedValues = new List<Test>
+			{
+				Test.A,
+				Test.B,
+				Test.C,
+			};
+			values.Should().BeEquivalentTo(expectedValues);
+		}
+	}
+}
