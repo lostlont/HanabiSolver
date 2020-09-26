51afbfeba22fdb672f470c31279b38338f6d8b59
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2020-09-26 09:58:59 +0200

    Using local constants for suite values in test.

--- HanabiSolver.Library.Tests/PlayerTests.Play.cs
+++ HanabiSolver.Library.Tests/PlayerTests.Play.cs
@@ -11,30 +11,32 @@ namespace HanabiSolver.Library.Tests
 		[Fact]
 		public void PlayOneDropsToTopOfCorrespondingEmptySuite()
 		{
-			var cardToPlay = new Card(Suite.Blue, Number.One);
+			const Suite suite = Suite.Blue;
+			var cardToPlay = new Card(suite, Number.One);
 
 			playerBuilder.Cards = cardToPlay.AsEnumerable();
 			var player = playerBuilder.Build();
 
 			player.Play(cardToPlay);
 
-			player.Table.PlayedCards[Suite.Blue].Top.Should().Be(cardToPlay);
+			player.Table.PlayedCards[suite].Top.Should().Be(cardToPlay);
 		}
 
 		[Fact]
 		public void PlayNextDropsToTopOfCorrespondingSuiteWithFollowingCardOnTop()
 		{
-			var cardsPlayed = new Card(Suite.Blue, Number.One).AsEnumerable();
-			var cardToPlay = new Card(Suite.Blue, Number.Two);
+			const Suite suite = Suite.Blue;
+			var cardsPlayed = new Card(suite, Number.One).AsEnumerable();
+			var cardToPlay = new Card(suite, Number.Two);
 
-			playerBuilder.TableBuilder.PlayedCardsBuilder[Suite.Blue] = () => new Pile(cardsPlayed);
+			playerBuilder.TableBuilder.PlayedCardsBuilder[suite] = () => new Pile(cardsPlayed);
 			playerBuilder.Cards = cardToPlay.AsEnumerable();
 			var player = playerBuilder.Build();
 
 			player.Play(cardToPlay);
 
 			var expectedCards = cardsPlayed.Append(cardToPlay);
-			player.Table.PlayedCards[Suite.Blue].Cards.Should().BeEquivalentTo(expectedCards);
+			player.Table.PlayedCards[suite].Cards.Should().BeEquivalentTo(expectedCards);
 		}
 
 		[Theory]
@@ -42,17 +44,18 @@ namespace HanabiSolver.Library.Tests
 		[InlineData(Number.Two, null)]
 		public void PlayWrongNumberGivesFuseTokenAndDiscardsCard(Number playedNumber, Number? lastNumber)
 		{
-			var cardToPlay = new Card(Suite.Blue, playedNumber);
+			const Suite suite = Suite.Blue;
+			var cardToPlay = new Card(suite, playedNumber);
 			var cardsPlayed = lastNumber
 				.ExistingAsEnumerable()
-				.Select(n => new Card(Suite.Blue, n));
+				.Select(n => new Card(suite, n));
 
-			playerBuilder.TableBuilder.PlayedCardsBuilder[cardToPlay.Suite] = () => new Pile(cardsPlayed);
+			playerBuilder.TableBuilder.PlayedCardsBuilder[suite] = () => new Pile(cardsPlayed);
 			var player = playerBuilder.Build();
 
 			player.Play(cardToPlay);
 
-			player.Table.PlayedCards[cardToPlay.Suite].Cards.Should().BeEquivalentTo(cardsPlayed);
+			player.Table.PlayedCards[suite].Cards.Should().BeEquivalentTo(cardsPlayed);
 			player.Table.FuseTokens.Amount.Should().Be(1);
 			player.Table.DiscardPile.Top.Should().Be(cardToPlay);
 		}
