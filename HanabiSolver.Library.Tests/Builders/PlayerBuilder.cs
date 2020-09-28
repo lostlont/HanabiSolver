3603694c53a728440c0330b195ff27fe5e58aa77
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2020-09-28 20:10:59 +0200

    PlayerBuilder.InformationBuilder

--- HanabiSolver.Library.Tests/Builders/PlayerBuilder.cs
+++ HanabiSolver.Library.Tests/Builders/PlayerBuilder.cs
@@ -9,12 +9,14 @@ namespace HanabiSolver.Library.Tests.Builders
 	{
 		public Func<IEnumerable<Card>> CardsBuilder { get; set; } = () => Enumerable.Empty<Card>();
 		public TableBuilder TableBuilder { get; set; } = new TableBuilder();
+		public Func<IEnumerable<Card>, Dictionary<Card, Information>> InformationBuilder { get; set; } = cards => cards.ToDictionary(card => card, card => new Information());
 
 		public Player Build()
 		{
 			var cards = CardsBuilder();
 			var table = TableBuilder.Build();
-			return new Player(cards, table);
+			var information = InformationBuilder(cards);
+			return new Player(cards, table, information);
 		}
 	}
 }
