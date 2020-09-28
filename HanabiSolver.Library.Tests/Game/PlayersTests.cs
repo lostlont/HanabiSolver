bb210e88aafcec9c03082cfd2e4ff6b292e8e76c
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2020-09-28 16:46:20 +0200

    Players as a separate collection class.

---
+++ HanabiSolver.Library.Tests/Game/PlayersTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using HanabiSolver.Library.Game;
+using HanabiSolver.Library.Tests.Builders;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace HanabiSolver.Library.Tests.Game
+{
+	public class PlayersTests
+	{
+		[Fact]
+		public void ConstructorSetsPlayers()
+		{
+			var table = new TableBuilder().Build();
+			var threePlayerList = new List<Player>
+			{
+				new Player(Enumerable.Empty<Card>(), table),
+				new Player(Enumerable.Empty<Card>(), table),
+				new Player(Enumerable.Empty<Card>(), table),
+			};
+			var players = new Players(threePlayerList);
+
+			players.Should().Equal(threePlayerList);
+		}
+	}
+}
---
+++ HanabiSolver.Library/Game/Players.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HanabiSolver.Library.Game
+{
+	public class Players : IReadOnlyList<Player>
+	{
+		private readonly List<Player> players;
+
+		public Players(IEnumerable<Player> players)
+		{
+			this.players = players.ToList();
+		}
+
+		public Player this[int index] => players[index];
+		public int Count => players.Count;
+
+		public IEnumerator<Player> GetEnumerator() => players.GetEnumerator();
+
+		IEnumerator IEnumerable.GetEnumerator() => players.GetEnumerator();
+	}
+}
