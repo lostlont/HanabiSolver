9006d65a157c1685fccd482e560c51d45e6d3b6a
Perjéssy Lóránt <lostlont@users.noreply.github.com>
2020-10-23 22:34:32 +0200

    Basics of GameManager.

---
+++ HanabiSolver.Library.Tests/Game/GameManagerTests.cs
@@ -0,0 +1,37 @@
+﻿using FluentAssertions;
+using HanabiSolver.Library.Game;
+using HanabiSolver.Library.Tests.Builders;
+using Moq;
+using Xunit;
+
+namespace HanabiSolver.Library.Tests.Game
+{
+	public class GameManagerTests
+	{
+		[Fact]
+		public void SimulateReturnsImmediatelyForFinishedState()
+		{
+			var fuseTokens = new Mock<ITokens>(MockBehavior.Strict);
+			fuseTokens
+				.Setup(t => t.MaxAmount)
+				.Returns(3);
+			fuseTokens
+				.Setup(t => t.Amount)
+				.Returns(3);
+			var gameState = new GameStateBuilder
+			{
+				Table = new TableBuilder
+				{
+					FuseTokens = fuseTokens.Object,
+				}.Build(),
+			}.Build();
+			var gameManager = new GameManager(gameState);
+
+			gameManager.Simulate();
+
+			gameState.IsEnded.Should().BeTrue();
+		}
+
+		// TODO Tests for simulating with non-finished states.
+	}
+}
---
+++ HanabiSolver.Library/Game/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HanabiSolver.Library.Game
+{
+	public class GameManager
+	{
+		public GameManager(GameState gameState)
+		{
+			GameState = gameState;
+		}
+
+		public GameState GameState { get; }
+
+		public void Simulate()
+		{
+		}
+	}
+}
